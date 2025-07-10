f43376479569303a9e28bf312d951e3f675133d5
Jeff Jennings <jjennings@flatironinstitute.org>
2025-07-10 11:42:24 -0400

    execute error catch

--- scripts/execute-convert.py
+++ scripts/execute-convert.py
@@ -1,41 +1,73 @@
-import os
+import subprocess
 import sys
 import glob
 
 slug = sys.argv[1]
 
+
+def run_command(command, verbose=0):
+    try:
+        result = subprocess.run(command, check=True, capture_output=True, text=True)
+    except (
+        subprocess.CalledProcessError
+    ) as e:  # Catching the error if the command fails
+        print(f"Error building book: {e.stderr}")
+        sys.exit(1)
+    if verbose > 0:
+        print(f"Output: {result.stdout}")
+    return result
+
+
 book = False
 notebooks = glob.glob("*.ipynb")
 if len(notebooks) > 1:
     print(
-        "More than one .ipynb notebook found --> assuming this is a book rather than a single notebook"
+        "More than one .ipynb notebook found --> assuming this is a book"
     )
     book = True
 
 if book is True:
     # build book
-    command = "jb build . --config _config.yml --toc _toc.yml"
-    os.system(command)
+    command = ["jb", "build", ".", "--config", "_config.yml", "--toc", "_toc.yml"]
+    run_command(command)
     # build pdf
-    command = "jb build . --config _config.yml --toc _toc.yml --builder pdfhtml"
-    os.system(command)
+    command = [
+        "jb",
+        "build",
+        ".",
+        "--config",
+        "_config.yml",
+        "--toc",
+        "_toc.yml",
+        "--builder",
+        "pdfhtml",
+    ]
+    run_command(command)
 
     # copy build outputs to 'html' dir
-    command = "cp -r '_build/html' html"
-    os.system(command)
-    command = "cp '_build/pdf/book.pdf' html"
-    os.system(command)
+    command = ["cp", "-r", "_build/html", "html"]
+    run_command(command)
+    command = ["cp", "_build/pdf/book.pdf", "html"]
+    run_command(command)
 
 else:
     # build single notebook
-    command = f"jb build {slug}.ipynb --config _config.yml"
-    os.system(command)
+    command = ["jb", "build", f"{slug}.ipynb", "--config", "_config.yml"]
+    run_command(command)
     # build pdf
-    command = f"jb build {slug}.ipynb --config _config.yml --builder pdfhtml"
-    os.system(command)
+    command = [
+        "jb",
+        "build",
+        f"{slug}.ipynb",
+        "--config",
+        "_config.yml",
+        "--builder",
+        "pdfhtml",
+    ]
+    run_command(command)
 
     # copy build outputs to 'html' dir
-    command = f"cp -r '_build/_page/{slug}/html' html"
-    os.system(command)
-    command = f"cp '_build/_page/{slug}/pdf/{slug}.pdf' html"
-    os.system(command)
+    command = ["cp", "-r", f"_build/_page/{slug}/html", "html"]
+    run_command(command)
+    command = ["cp", f"_build/_page/{slug}/pdf/{slug}.pdf", "html"]
+    run_command(command)
