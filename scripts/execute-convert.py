489b98bc043533c088f0cd92297a4ecdc7983394
Jeff Jennings <jjennings@flatironinstitute.org>
2025-10-20 12:51:14 -0400

    simplify build, restoring rendered page features (buttons, section navigation)

--- scripts/execute-convert.py
+++ scripts/execute-convert.py
@@ -17,55 +17,23 @@ def run_command(command, verbose=1):
         print(f"Output: {result.stdout}")
     return result
 
-
-book = False
 notebooks = glob.glob("*.ipynb")
 if len(notebooks) > 1:
     print("More than one .ipynb notebook found --> assuming this is a book")
-    book = True
 
-if book is True:
     # build book
     command = ["jb", "build", ".", "--config", "_config.yml", "--toc", "_toc.yml"]
-    run_command(command)
-    # build pdf
-    command = [
-        "jb",
-        "build",
-        ".",
-        "--config",
-        "_config.yml",
-        "--toc",
-        "_toc.yml",
-        "--builder",
-        "pdfhtml",
-    ]
     run_command(command)
 
     # copy build outputs to 'html' dir
     command = ["cp", "-r", "_build/html", "html"]
     run_command(command)
-    command = ["cp", "_build/pdf/book.pdf", "html"]
-    run_command(command)
-
+    
 else:
     # build single notebook
     command = ["jb", "build", f"{slug}.ipynb", "--config", "_config.yml"]
     run_command(command)
-    # build pdf
-    command = [
-        "jb",
-        "build",
-        f"{slug}.ipynb",
-        "--config",
-        "_config.yml",
-        "--builder",
-        "pdfhtml",
-    ]
-    run_command(command)
 
     # copy build outputs to 'html' dir
     command = ["cp", "-r", f"_build/_page/{slug}/html", "html"]
-    run_command(command)
-    command = ["cp", f"_build/_page/{slug}/pdf/{slug}.pdf", "html"]
     run_command(command)
